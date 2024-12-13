018dae237e4997cf7cecb46087a497735bb7d822
MOJAN <sjs732@naver.com>
2024-12-13 17:28:03 +0900

    Feat: 백엔드 API에 연결, 로딩 추가

--- cfind-front/src/pages/data/Data.tsx
+++ cfind-front/src/pages/data/Data.tsx
@@ -4,10 +4,12 @@ import DataFilter from "./DataFilter";
 import { FilterContext } from "./FilterContext";
 import { useContext, useEffect, useState } from "react";
 import DataElement from "./DataElement";
-import { tableData } from "../../../table";
+// import { tableData } from "../../../table";
 import { useSort } from "../../hooks/useSort";
 import SortIcon from "../../components/SortIcon";
 import { GSMData } from "../../types";
+import axios from "axios";
+import Loading from "../../assets/images/loading.svg"
 
 const BackgroundInfo = styled.div`
     display: flex;
@@ -72,18 +74,50 @@ const DataHeader = styled.section`
     }
 `
 
+const LoadingIndicator = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    left: 0;
+    transform: translateY(-16px);
+    backdrop-filter: blur(4px);
+    justify-content: center;
+    img{
+        height: ${({theme}) => theme.size.xl}; 
+        width: ${({theme}) => theme.size.xl};
+    }
+`
+
 type DataFilterType = {
     column: string;
     valueList: Array<string>;
     valueCount: Array<number>;
 }
 
+type DataFilterGroupType = {
+    [key: string]: DataFilterType
+}
+
 const Data = () => {
+
     const { filter } = useContext(FilterContext)!;
 
-    const [DataList, sortDataList, setDataList] = useSort(tableData);
+    const [DataList, sortDataList, setDataList] = useSort<GSMData>([]);
 
-    const OriginalData = tableData;
+    const [OriginalData, setOriginalData] = useState<GSMData[]>([]);
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_BACKEND_URL + "/gsmdata").then((res) => {
+            const DataList = res.data;
+            console.log(DataList);
+            setDataList(DataList)
+            setOriginalData(DataList)
+            setIsLoading(false);
+        })
+    }, [])
 
     useEffect(() => {
         const filterList = Object.entries(filter).filter(([_, value]) => value.size > 0);
@@ -96,35 +130,37 @@ const Data = () => {
         setDataList([...filteredList])
     }, [filter]);
 
-    const DataFilterGroup:{
-        [key: string]: DataFilterType
-    } = {};
-
-    Object.entries(tableData[0]).forEach(([key, _]) => {
-        DataFilterGroup[key] = {
-            column: key,
-            valueList: [],
-            valueCount: []
-        }
-    })
-    tableData.forEach((table) => {
-        Object.entries(table).forEach(([key, value]) => {
-            const index = DataFilterGroup[key].valueList.indexOf(value);
-            if(index == -1){
-                DataFilterGroup[key].valueList.push(value);
-                DataFilterGroup[key].valueCount.push(1);
-            }
-            else{
-                DataFilterGroup[key].valueCount[index]++;
-            }
+    useEffect(() => {
+        const DataFilterGroup: DataFilterGroupType = {}
+        OriginalData.forEach((table) => {
+            Object.entries(table).forEach(([key, value]) => {
+                if(!Object.keys(DataFilterGroup).includes(key)){
+                    DataFilterGroup[key] = {
+                        column: key,
+                        valueList: [],
+                        valueCount: []
+                    }
+                }
+                const index = DataFilterGroup[key].valueList.indexOf(value);
+                if(index == -1){
+                    DataFilterGroup[key].valueList.push(value);
+                    DataFilterGroup[key].valueCount.push(1);
+                }
+                else{
+                    DataFilterGroup[key].valueCount[index]++;
+                }
+            })
         })
-    })
+        delete DataFilterGroup['GSM_ID']
+        delete DataFilterGroup['GSE_ID']
+        setDataFilterGroup(DataFilterGroup)
+    }, [OriginalData])
 
-    delete DataFilterGroup["GSE_ID"]
-    delete DataFilterGroup["GSM_ID"]
+    const [DataFilterGroup, setDataFilterGroup] = useState<DataFilterGroupType>({})
+
 
     const [sortBy, setSortBy] = useState(0);
-    const sortList = ["GSM ID", "GSE ID", "Disease", "Tissue", "Genotype", "Replicate Number", "age", "species"]
+    const sortList = ["GSM ID", "GSE ID", "Disease", "Tissue", "Genotype", "Replicate Number", "age", "gender", "LVEF", "species"]
     const compareFuncList: ((a: GSMData, b: GSMData) => number)[] = [
         (a, b) => a.GSM_ID > b.GSM_ID ? 1 : -1,
         (a, b) => a.GSM_ID < b.GSM_ID ? 1 : -1,
@@ -140,6 +176,10 @@ const Data = () => {
         (a, b) => a.replicate_num < b.replicate_num ? 1 : -1,
         (a, b) => a.age > b.age ? 1 : -1,
         (a, b) => a.age < b.age ? 1 : -1,
+        (a, b) => a.gender > b.gender ? 1 : -1,
+        (a, b) => a.gender < b.gender ? 1 : -1,
+        (a, b) => a.lvef > b.lvef ? 1 : -1,
+        (a, b) => a.lvef < b.lvef ? 1 : -1,
         (a, b) => a.species > b.species ? 1 : -1,
         (a, b) => a.species < b.species ? 1 : -1
     ]
@@ -147,6 +187,8 @@ const Data = () => {
     useEffect(() => {
         sortDataList(compareFuncList[sortBy]);
     }, [sortBy])
+
+    const filterKey = ["species", "disease", "tissue", "genotype", "replicate_num", "age", "gender", "lvef"]
 
     return (
         <>
@@ -161,16 +203,25 @@ const Data = () => {
                 </BackgroundInfo>
             </SmallBackground>
             <DataLayout>
-                    <FilterSection> 
-                        <h1>
-                            Filter
-                        </h1>
-                        {Object.entries(DataFilterGroup).map(([_, value]) => {
-                            return(
-                                <DataFilter key={value.column} {...value} />
+                {isLoading && 
+                    <LoadingIndicator>
+                        <img src={Loading}/>
+                    </LoadingIndicator>
+                }
+                <FilterSection> 
+                    <h1>
+                        Filter
+                    </h1>
+                    {
+                        Object.values(DataFilterGroup).sort((a, b) => {
+                            return filterKey.indexOf(a.column) - filterKey.indexOf(b.column)
+                        }).map((value) => {
+                            return (
+                                <DataFilter key={value.column} {...value}/>
                             )
-                        })}
-                    </FilterSection>
+                        })
+                    }
+                </FilterSection>
                 <DataSection>
                     <DataHeader>
                         {sortList.map((value, index) => {
@@ -182,9 +233,9 @@ const Data = () => {
                             )
                         })}
                     </DataHeader>
-                    {DataList.map((table) => {
+                    {DataList.map((table, index) => {
                         return(
-                            <DataElement key={table.GSM_ID} {...table} />
+                            <DataElement key={table.GSM_ID + index} {...table} />
                         )
                     })}
                 </DataSection>
