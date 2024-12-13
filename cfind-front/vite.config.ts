0004d7c6c105b3a7ff5a8b9c28d1b8288d9404d3
MOJAN <sjs732@naver.com>
2024-12-13 17:29:49 +0900

    Chore: vite config 수정

--- cfind-front/vite.config.ts
+++ cfind-front/vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy: {
-      '/api' : 'http://localhost:3000'
-    }
-  }
-})
+export default ({ mode }: {mode: string}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+      plugins: [react()],
+      define: {
+        __APP_ENV__: JSON.stringify(env.APP_ENV),
+      },
+      build: {
+        minify: false
+      },
+      server: {
+          proxy: {
+              '/api': {
+                  // target: `${env.BACKEND_URL}`,
+                  changeOrigin: true,
+                  rewrite: (path) => path.replace(/^\/api/, '')
+              },
+          },
+      },
+  });
+};
